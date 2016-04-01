15f666b6d1040cbfa2e3e5528e81c2e38037849b
khalil ait ammar <ait.ammar.khalil@gmail.com>
2016-04-01 11:51:12 +0200

    ajout des styles et du premier controller

--- src/controllers/categoryCont.js
+++ src/controllers/categoryCont.js
@@ -1,18 +1,14 @@
 'use strict'
-var categories = [
-  {'id':1, 'name':'films'},
+var infokhalil = [
+  {'id':'ait.ammar.khalil@gmail.com', 'num':21938926 , 'Personal Profile':'khalil'},
     {'id':2, 'name': 'musiques'}
 ];
 
-app.controller('categoryIndex',function ($scope) {
+app.controller('infocraft',function ($scope) {
+  $scope.mail="khalil.ait.ammar@gmail.com";
+  $scope.num=21938926;
+  $scope.infokhalil='Jeune diplomé, passionné par la technologie apprécie le travail en equipe. des compétences en leadership développées tout au long de plusieurs activités et responsabilités estudiantine et associative.ayant un grand intérêt pour la vie en entreprise developper aprés avoir effectuer mes deux stages de PFE respctivement chez ooredoo et la banque ATB pour ma licence et mon cycle dingenieur';
 
-})
-.controller('categoryList',function ($scope) {
-
-})
-.controller('categoryCreate',function ($scope) {
-  $scope.test="toto";
-})
-.controller('categoryRemove',function ($scope) {
+$scope.exp =' première expérience réussi dans une startup 6mois de dev chez adveez , dev front avec react';
 
 });
