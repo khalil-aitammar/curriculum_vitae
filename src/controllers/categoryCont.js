88f3b5759d8fcd85fbb901e6377bf9728ad90a00
khalil ait ammar <ait.ammar.khalil@gmail.com>
2016-04-01 10:36:22 +0200

    initialisation du projet

---
+++ src/controllers/categoryCont.js
@@ -0,0 +1,18 @@
+'use strict'
+var categories = [
+  {'id':1, 'name':'films'},
+    {'id':2, 'name': 'musiques'}
+];
+
+app.controller('categoryIndex',function ($scope) {
+
+})
+.controller('categoryList',function ($scope) {
+
+})
+.controller('categoryCreate',function ($scope) {
+  $scope.test="toto";
+})
+.controller('categoryRemove',function ($scope) {
+
+});
